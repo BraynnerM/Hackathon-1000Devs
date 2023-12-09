d64e4ef9b8f78afcb3ccf64c1f6e587baafbec48
reiskaren <kreisdemenezes@gmail.com>
2023-12-09 14:24:58 -0300

    add index

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 const express = require('express');
 const app = express();
-const PORT = 
+const PORT = process.env.PORT || 3000;
+const pool = require('./config/database');
 
+// Configuração do corpo da solicitação para trabalhar com JSON
+app.use(express.json());
+
+// Verificar conexão com o banco de dados
+pool.query('SELECT NOW()', (err, res) => {
+    if (err) {
+        console.error('Erro ao conectar ao banco de dados:', err);
+    } else {
+        console.log('Conexão bem-sucedida com o banco de dados.');
+    }
+});
+
+// Inicialização do servidor
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
 
 // Configuração das rotas
 const express = require('express');
 const database = require('./config/database');
 const routes = require('./src/routes/routes'); 
-
-const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
