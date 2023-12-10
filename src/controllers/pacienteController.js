d9141834987e97423c59e98067b7a52f40f5685a
Braynner Marques <braynnermarques@hotmail.com>
2023-12-10 01:56:54 -0300

    feat: requisito 2 concluido

--- src/controllers/pacienteController.js
+++ src/controllers/pacienteController.js
@@ -30,14 +30,44 @@ async function getPacienteById(req, res) {
   }
 }
 
+async function getPacienteByNome(req, res) {
+  const { nome } = req.params; // Obtém o valor do parâmetro da URL
+
+  try {
+    const paciente = await prisma.paciente.findMany({
+      where: {
+        nome: {
+          contains: nome, 
+          mode: 'insensitive', 
+        },
+      },
+    });
+
+    if (!paciente|| paciente.length === 0) {
+      res.status(404).send('Nenhum paciente encontrado com esse nome');
+    } else {
+      res.json(paciente);
+    }
+  } catch (error) {
+    console.error('Erro ao obter paciente por nome:', error);
+    res.status(500).send('Erro interno do servidor');
+  }
+}
+
 async function createPaciente(req, res) {
   const { nome, data_nascimento } = req.body;
 
   try {
-    const novoPaciente = await prisma.paciente.create({
+    // Consulta para obter a quantidade de IDs já gravados na tabela
+    const countResult = await prisma.paciente.count();
+    const quantidadeDeIds = countResult;
+
+    const novoId = quantidadeDeIds + 1;
+    const paciente = await prisma.paciente.create({
       data: {
-        nome,
-        data_nascimento,
+        nome: nome,  // Correção aqui
+        data_nascimento: data_nascimento,  // Correção aqui
+        id_paciente: novoId,
       },
     });
 
@@ -47,6 +77,7 @@ async function createPaciente(req, res) {
     res.status(404).json({ mensagem: 'Erro ao inserir paciente.' });
   }
 }
+
 
 async function updatePaciente(req, res) {
   const { id } = req.params;
@@ -86,6 +117,7 @@ async function deletePaciente(req, res) {
 module.exports = {
   getPacientes,
   getPacienteById,
+  getPacienteByNome,
   createPaciente,
   updatePaciente,
   deletePaciente,
--- src/controllers/vacinaAplicadaController.js
+++ src/controllers/vacinaAplicadaController.js
@@ -1,29 +1,122 @@
-import { PrismaClient } from '@prisma/client';
-
+// vacinaAplicadaController.js
+const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
-export const getVacinasAplicadas = async (req, res) => {
+// API de pesquisa de vacinas aplicadas por pessoa
+async function getVacinasAplicadas(req, res) {
   try {
-    const vacinasAplicadas = await prisma.vacinaAplicada.findMany();
-    res.status(200).json(vacinasAplicadas);
+    const vacinasAplicadas = await prisma.vacinaaplicada.findMany({
+      include: {
+        paciente: true, // Inclui dados relacionados ao paciente
+        vacina: true, // Inclui dados relacionados à vacina
+      },
+    });
+
+    // Mapeia os resultados para incluir apenas as informações desejadas
+    const resultadoFormatado = vacinasAplicadas.map((aplicacao) => ({
+      id_paciente: aplicacao.id_paciente,
+      id_vacina: aplicacao.id_vacina,
+      data_aplicacao: aplicacao.data_aplicacao,
+      nome_paciente: aplicacao.paciente.nome, // Obtém o nome do paciente
+      nome_vacina: aplicacao.vacina.vacina, // Obtém o nome da vacina
+    }));
+
+    res.status(200).json(resultadoFormatado);
   } catch (error) {
     console.error(error);
     res.status(404).json({ mensagem: 'Erro ao executar a consulta.' });
   }
 };
 
-export const getVacinaAplicadaById = async (req, res) => {
-  // Implemente a lógica para buscar uma vacina aplicada por ID
-};
+// API de pesquisa de vacina aplicada por ID
+async function getVacinaAplicadaById(req, res) {
+  const id = parseInt(req.params.id);
 
-export const createVacinaAplicada = async (req, res) => {
-  // Implemente a lógica para criar uma nova vacina aplicada
-};
+  try {
+    const vacinaAplicada = await prisma.vacinaaplicada.findUnique({
+      where: { id },
+    });
 
-export const updateVacinaAplicada = async (req, res) => {
-  // Implemente a lógica para atualizar uma vacina aplicada por ID
-};
+    if (!vacinaAplicada) {
+      res.status(404).send('Vacina aplicada não encontrada');
+    } else {
+      res.json(vacinaAplicada);
+    }
+  } catch (error) {
+    console.error('Erro ao obter vacina aplicada por ID:', error);
+    res.status(500).send('Erro interno do servidor');
+  }
+}
 
-export const deleteVacinaAplicada = async (req, res) => {
-  // Implemente a lógica para excluir uma vacina aplicada por ID
+// API de cadastro de vacina aplicada
+async function createVacinaAplicada(req, res) {
+  const { id_paciente, id_vacina, data_aplicacao } = req.body;
+
+  try {
+    // Verificar se o paciente existe antes de criar a vacina aplicada
+    const paciente = await prisma.paciente.findUnique({
+      where: { id_paciente: id_paciente },
+    });
+
+    if (!paciente) {
+      return res.status(404).json({ mensagem: 'Paciente não encontrado.' });
+    }
+
+    const vacinaAplicada = await prisma.vacinaaplicada.create({
+      data: {
+        id_paciente,
+        id_vacina,
+        data_aplicacao: new Date(data_aplicacao),
+      },
+    });
+
+    res.status(201).json(vacinaAplicada);
+  } catch (error) {
+    console.error('Erro ao cadastrar vacina aplicada:', error);
+    res.status(500).send('Erro interno do servidor');
+  }
+}
+
+// API de atualização de vacina aplicada
+async function updateVacinaAplicada(req, res) {
+  const id = parseInt(req.params.id);
+  const dadosAtualizados = req.body;
+
+  try {
+    const vacinaAplicada = await prisma.vacinaaplicada.update({
+      where: { id },
+      data: dadosAtualizados,
+    });
+
+    res.json(vacinaAplicada);
+  } catch (error) {
+    console.error('Erro ao atualizar vacina aplicada:', error);
+    res.status(500).send('Erro interno do servidor');
+  }
+}
+
+async function deleteVacinaAplicada(req, res) {
+  const id_paciente = parseInt(req.params.id);
+  const id_vacina = parseInt(req.params.idVacina);
+
+  try {
+    await prisma.vacinaaplicada.delete({
+      where: { id_paciente_id_vacina: { id_paciente, id_vacina } },
+    });
+
+    res.send('Vacina aplicada excluída com sucesso');
+  } catch (error) {
+    console.error('Erro ao excluir vacina aplicada:', error);
+    res.status(500).send('Erro interno do servidor');
+  }
+}
+
+
+
+module.exports = {
+  getVacinasAplicadas,
+  getVacinaAplicadaById,
+  createVacinaAplicada,
+  updateVacinaAplicada,
+  deleteVacinaAplicada,
 };
