ca540a7edb1f6b8b2a8f7c5c6744623f5449d34c
reiskaren <kreisdemenezes@gmail.com>
2023-12-09 22:06:27 -0300

    docs: add controller routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -12,5 +12,32 @@ router.delete('/campanhas/:id', deleteCampanha);
 
 // Rotas da Paciente
 
+// Rotas da Vacina
+router.get('/vacinas', getVacinas);
+router.get('/vacinas/:id', getVacinaById);
+router.post('/vacinas', createVacina);
+router.put('/vacinas/:id', updateVacina);
+router.delete('/vacinas/:id', deleteVacina);
+
+// Rotas da Rede
+router.get('/redes', getRedes);
+router.get('/redes/:id', getRedeById);
+router.post('/redes', createRede);
+router.put('/redes/:id', updateRede);
+router.delete('/redes/:id', deleteRede);
+
+// Rotas de Período Aplicação Mês
+router.get('/periodos', getPeriodos);
+router.get('/periodos/:id', getPeriodoById);
+router.post('/periodos', createPeriodo);
+router.put('/periodos/:id', updatePeriodo);
+router.delete('/periodos/:id', deletePeriodo);
+
+// Rotas de Período Aplicação Ano
+router.get('/periodos', getPeriodos);
+router.get('/periodos/:id', getPeriodoById);
+router.post('/periodos', createPeriodo);
+router.put('/periodos/:id', updatePeriodo);
+router.delete('/periodos/:id', deletePeriodo);
 
 export default router;
